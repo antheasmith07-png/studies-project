3c6d81ab7e41bea6b3447df2dc4672efc42f1fdb
antheasmith07-png <anthea.smith07@gmail.com>
2026-05-31 20:23:38 -0500

    AS_controlflow.js_5.31.26

--- control-flow.js
+++ control-flow.js
@@ -2,5 +2,21 @@
  * CONTROL FLOW:
  * 
  * 0: (introduction)
+ * Control Flow is how the javascript program is executed. 
+ * The flow of the program is usually executed in the order from top down. 
+ * There is also conditional flow which is executed based on conditional 
+ * statements and loops that requires certain conditions to be met before certain code is ran.
  * 
+ * 1:To see how control flow operate you have to have more than one statement or set of code.
+ * 
+ * 2:To see the order of operation you can log the statements to the console.
  */
+
+//1. Need more than one block of code
+    var name = Anthea;
+    var hey = "hey" + name;
+    
+//2. log the code to the console, 
+//but this would show up on our program output in the order above
+console.log(name);
+console.log(hey);
